298a82451daa56239938f31621a34f9293a347bd
Bruno Costa <bruno.costa@dafiti.com.br>
2023-10-11 16:11:54 -0300

    wip

--- src/Data.php
+++ src/Data.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Costa\Entity;
 
 use Carbon\Carbon;
-use Costa\Entity\Contracts\DataInterface;
 use Costa\Entity\Traits\FromTrait;
 use Costa\Entity\Traits\MethodMagicsTrait;
 use Costa\Entity\ValueObject\Uuid;
