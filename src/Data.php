b202cd968e1489af572a583dc9b9f05495903f66
Bruno Costa <bruno.costa@dafiti.com.br>
2023-10-11 18:29:39 -0300

    wip

--- src/Data.php
+++ src/Data.php
@@ -4,11 +4,32 @@ declare(strict_types=1);
 
 namespace Costa\Entity;
 
-use Costa\Entity\Traits\EntityTrait;
 use Costa\Entity\Traits\MethodMagicTrait;
+use Costa\Entity\ValueObject\Uuid;
+use DateTime;
+use DateTimeInterface;
 
 abstract class Data
 {
     use MethodMagicTrait;
-    use EntityTrait;
+    protected readonly Uuid $id;
+
+    protected readonly DateTimeInterface $createdAt;
+
+    protected DateTimeInterface $updatedAt;
+
+    protected function generateId(): Uuid
+    {
+        return Uuid::make();
+    }
+
+    protected function generateCreatedAt(): DateTimeInterface
+    {
+        return new DateTime();
+    }
+
+    protected function generateUpdatedAt(): DateTimeInterface
+    {
+        return new DateTime();
+    }
 }
